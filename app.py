2b65a1a3487df17d61b6c8bbb1ac5ddfe2afa12b
dev8psST <88630556+dev8psST@users.noreply.github.com>
2025-03-17 12:59:58 -0400

    Update app.py
    change path to db file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,flash, redirect,url_for,session,logging,
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/bozkurt/Desktop/login-register-form/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////...your_path.../database.db'
 db = SQLAlchemy(app)
 
 
